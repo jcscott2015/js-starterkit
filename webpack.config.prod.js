f58fd3bc560181825719bbcf2df62e59c244a65b
John C. Scott <jcscott@scottcomm.com>
2017-11-07 00:27:45 -0800

    Removed deprecated webpack-md5-hash.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import WebpackMd5Hash from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
@@ -17,8 +16,8 @@ export default {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        //filename: '[name].[chunkhash].js'
-        chunkFilename: "[chunkhash].[id].chunk.js"
+        filename: '[name].[chunkhash].js'
+        //chunkFilename: "[chunkhash].[id].chunk.js"
     },
     plugins: [
         new webpack.LoaderOptionsPlugin({
@@ -29,9 +28,6 @@ export default {
 
         // Generate an external css file with a hash in the filename
         new ExtractTextPlugin('[name].[contenthash].css'),
-
-        // Hash the files using MD5 so that their names change when the content changes.
-        new WebpackMd5Hash(),
 
         /* Use CommonsChunkPlugin to create a separate bundle of vendor libraries so that they're cached separately. */
         new webpack
