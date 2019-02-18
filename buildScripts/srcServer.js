bd08a205c420758d7a020c619cfe714455bda07e
John C. Scott <jcscott@scottcomm.com>
2019-02-17 22:01:26 -0800

    Updated and audited node packages.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,7 +3,10 @@ import path from 'path';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 import webpackMiddleware from 'webpack-dev-middleware';
-import { chalkError, chalkProcessing } from './chalkConfig';
+import {
+    chalkError,
+    chalkProcessing
+} from './chalkConfig';
 
 /* eslint-disable no-console */
 
@@ -14,54 +17,54 @@ app.use(express.static(path.join(__dirname, '../dist')));
 const bundler = webpack(config);
 
 app.use(webpackMiddleware(bundler, {
-  // Dev middleware can't access config, so we provide publicPath
-  publicPath: config.output.publicPath,
-  //contentBase: 'src',
-  /*These settings suppress noisy webpack output so only errors are displayed to the console.*/
-  //noInfo: true,
-  stats: {
-    assets: false,
-    colors: true,
-    version: false,
-    hash: false,
-    timings: false,
-    chunks: false,
-    chunkModules: false
-  },
+    // Dev middleware can't access config, so we provide publicPath
+    publicPath: config.output.publicPath,
+    //contentBase: 'src',
+    /*These settings suppress noisy webpack output so only errors are displayed to the console.*/
+    //noInfo: true,
+    stats: {
+        assets: false,
+        colors: true,
+        version: false,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false
+    },
 
-  // for other settings see
-  // https://webpack.js.org/guides/development/#using-webpack-dev-middleware
+    // for other settings see
+    // https://webpack.js.org/guides/development/#using-webpack-dev-middleware
 }));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.get('/users', function (req, res) {
-  // Hard coding for simplicity. Pretend this hits a real database
-  res.json([{
-    "id": 1,
-    "firstName": "Bob",
-    "lastName": "Smith",
-    "email": "bob@gmail.com"
+app.get('/users', function(req, res) {
+    // Hard coding for simplicity. Pretend this hits a real database
+    res.json([{
+        "id": 1,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@gmail.com"
     }, {
-    "id": 2,
-    "firstName": "Tammy",
-    "lastName": "Norton",
-    "email": "tnorton@yahoo.com"
+        "id": 2,
+        "firstName": "Tammy",
+        "lastName": "Norton",
+        "email": "tnorton@yahoo.com"
     }, {
-    "id": 3,
-    "firstName": "Tina",
-    "lastName": "Lee",
-    "email": "lee.tina@hotmail.com"
+        "id": 3,
+        "firstName": "Tina",
+        "lastName": "Lee",
+        "email": "lee.tina@hotmail.com"
     }]);
 });
 
 app.listen(port, 'localhost', err => {
-  if (err) {
-    console.error(chalkError(err));
-    return;
-  }
+    if (err) {
+        console.error(chalkError(err));
+        return;
+    }
 
-  console.log(chalkProcessing(`Listening at http://localhost:${port}`));
+    console.log(chalkProcessing(`Listening at http://localhost:${port}`));
 });
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,66 +4,66 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  mode: 'development',
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.json', 'map']
-  },
-  devtool: 'inline-source-map',
-  target: 'web',
-  output: {
-    filename: 'bundle.js',
-    // path: path.resolve(__dirname, './dist'), // Preview build, if available. Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/'
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: false,
-      debug: true,
-      noInfo: true // set to false to see a list of every file being bundled.
-    }),
+    mode: 'development',
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.json', 'map']
+    },
+    devtool: 'inline-source-map',
+    target: 'web',
+    output: {
+        filename: 'bundle.js',
+        // path: path.resolve(__dirname, './dist'), // Preview build, if available. Note: Physical files are only output by the production build task `npm run build`.
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: false,
+            debug: true,
+            noInfo: true // set to false to see a list of every file being bundled.
+        }),
 
-    // Create HTML file that includes reference to bundled JS.
-    new HtmlWebpackPlugin({ // Create HTML file that includes references to bundled CSS and JS.
-      title: 'JS Starter Kit',
-      template: './src/index.html',
-      filename: "./index.html",
-      xhtml: true
-    }),
+        // Create HTML file that includes reference to bundled JS.
+        new HtmlWebpackPlugin({ // Create HTML file that includes references to bundled CSS and JS.
+            title: 'JS Starter Kit',
+            template: './src/index.html',
+            filename: "./index.html",
+            xhtml: true
+        }),
 
-    new ExtractTextPlugin("css/index.css"),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-],
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.(png|jpg|svg)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'file-loader'
-      },
-    }, {
-      test: /(\.css|\.scss|\.sass)$/,
-      exclude: /node_modules/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            minimize: true,
-            sourceMap: true
-          }
+        new ExtractTextPlugin("css/index.css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
         }, {
-          loader: 'sass-loader',
-          options: {
-            includePaths: [path.resolve(__dirname, './src', './scss')],
-            sourceMap: true
-          }
+            test: /\.(png|jpg|svg)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'file-loader'
+            },
+        }, {
+            test: /(\.css|\.scss|\.sass)$/,
+            exclude: /node_modules/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [{
+                    loader: 'css-loader'
+                        // options: {
+                        //   minimize: true,
+                        //   sourceMap: true
+                        // }
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: [path.resolve(__dirname, './src', './scss')],
+                        sourceMap: true
+                    }
+                }]
+            })
         }]
-      })
-    }]
-  }
+    }
 }
